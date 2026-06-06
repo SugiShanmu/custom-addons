53d650d52d2a6bb6da44102bff701fd7d9e39be5
SugiShanmu <shanmu4u@gmail.com>
2026-06-06 14:15:07 +0530

    Update fleet_ml_analytics.py

--- automotive_fleet_ml_analytics/models/fleet_ml_analytics.py
+++ automotive_fleet_ml_analytics/models/fleet_ml_analytics.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
+
+
 class FleetMLAnalytics(models.Model):
     """Model to store ML Analytics predictions for fleet vehicles."""
     _name = 'automotive.fleet.ml.analytics'
